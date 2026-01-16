ed6b50b63eaa96315a5876d00df93afbf56a8b1c
NaCryptoKe <NahomCryptoKetema@gmail.com>
2026-01-16 10:38:18 +0300

    Add example module and outline for Verilog learning roadmap

---
+++ example.v
@@ -0,0 +1,69 @@
+module example(
+    input clk,
+    input a,
+    input b, 
+    output c
+);
+
+    wire w;     // wire declaration
+    // Wire represents continous value, the default value of the net is Z which is high
+    reg r;      // reg declaration
+    // reg represents a value stored over time, used in procedural blocks. the default value of the net is X which is low
+    assign w = a & b;   // wire continous assignment
+
+    always @ (posedge clk) begin
+        r = a;  // reg procedure assignment
+    end
+
+    assign c = r ^ w;
+end module
+
+/*
+Operators in verilog
+
+Logical Operators
+-----------------
+&   bitwise AND
+|   bitwise OR
+^   bitwise XOR
+~   bitwise NOT
+^~  bitwise XNOR
+
+&&  boolean AND
+||  boolean OR
+!   boolean NOT
+
+Arithmetic Operation
+--------------------
++   addition
+-   subtraction
+
+Reduction Operators
+-------------------
+&   reduce via AND
+~&  reduce via NAND
+|   reduce via OR
+~|  reduce via NOR
+^   reduce via XOR
+^~  reduce via XNOR
+
+Shift Operators
+---------------
+>>  shift right
+<<  shift left
+>>> arithmetic shift right
+
+Relational Operators
+--------------------
+==  equal
+!=  not equal
+>   greather than
+>=  greater than equals to
+<   less than
+<=  less than or equals to
+
+Other Operators
+---------------
+{}      concatenate
+{N{}}   replicate N times
+*/
